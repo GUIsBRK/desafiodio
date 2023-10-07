aac0f5896b38cafde0ef5fe9e0347cd1440cb772
Guilherme Vieira Fernandes <guilhermevides2002@gmail.com>
2023-10-07 18:24:21 -0300

    atualizando

---
+++ desafiodioIPhone/src/IPhone.java
@@ -0,0 +1,39 @@
+// Classe iPhone que implementa as interfaces
+public class IPhone implements ReprodutorMusical, AparelhoTelefonico, NavegadorInternet {
+    // Implementação dos métodos das interfaces
+    public void tocar() {
+        // Lógica para tocar música
+    }
+
+    public void pausar() {
+        // Lógica para pausar música
+    }
+
+    public void selecionarMusica(String musica) {
+        // Lógica para selecionar uma música
+    }
+
+    public void ligar() {
+        // Lógica para ligar o telefone
+    }
+
+    public void atender() {
+        // Lógica para atender uma chamada
+    }
+
+    public void iniciarCorreioVoz() {
+        // Lógica para iniciar o correio de voz
+    }
+
+    public void exibirPagina(String url) {
+        // Lógica para exibir uma página na internet
+    }
+
+    public void adicionarNovaAba(String url) {
+        // Lógica para adicionar uma nova aba no navegador
+    }
+
+    public void atualizarPagina() {
+        // Lógica para atualizar a página no navegador
+    }
+}
